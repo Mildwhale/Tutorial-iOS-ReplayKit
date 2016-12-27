384c53f1bb0a662c50418fbee73dca299778d1c0
KyuJin Kim <kjkim@gamevilcom2us.com>
2016-12-27 15:52:50 +0900

    Xcode project initial commit.

---
+++ ReplayKitTutorial-ObjC/ReplayKitTutorial-ObjC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ReplayKitTutorial-ObjC
+//
+//  Created by KyuJin Kim on 2016. 12. 27..
+//  Copyright © 2016년 KyuJin Kim. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
